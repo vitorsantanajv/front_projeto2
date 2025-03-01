877877907b56511082bd4f2ac60f8606629276b9
Vitor Santana <vitorferaff2@gmail.com>
2025-03-01 10:36:36 -0300

    primeiro commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+
+function navigateTo(page) {
+    window.location.href = page;
+}
+
+function openGraph() {
+    window.open('https://docs.google.com/forms/d/1L_mJ0MtxCfUaieo-87zMVfGtJFfszUNcXfOBBTpkZc8/edit?ts=6743d9e4#responses', '_blank');
+}
